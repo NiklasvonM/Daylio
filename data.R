b25b9a26c84221380352c8a6145155759d6af85e
nniklasvm <niklasvmoers@t-online.de>
2021-01-30 16:24:09 +0100

    init: data parsing

---
+++ data.R
@@ -0,0 +1,29 @@
+library(data.table)
+library(lubridate)
+
+dtOrig <- fread("daylio_export_2021_01_30.csv", encoding = "UTF-8")
+dt <- dtOrig[, .(
+  Datum = as.Date(full_date),
+  Wochentag = weekday,
+  Stimmung = mood,
+  Activities = activities
+)]
+dt[, Stimmung := sapply(Stimmung, function(x) switch(x,
+  "Super" = 5,
+  "Gut" = 4,
+  "Ok" = 3,
+  "Schlecht" = 2,
+  "Lausig" = 1
+))]
+
+activities <- paste0(dt$Activities, collapse = " | ")
+activities <- base::strsplit(activities, split = " | ", fixed = TRUE)
+activities <- unlist(activities)
+activities <- unique(activities)
+for (activity in activities) {
+  dt[, (activity) := as.integer(grepl(activity, Activities))]
+}
+dt[, Activities := NULL]
+dt[, Schlafqualitaet := (-1 * Schlecht + 1 * Gut) / (Schlecht + Mäßig + Gut)]
+dt[, `Koerperliche Taetigkeiten` := Yoga + Sport + Laufen]
+fwrite(dt, file = paste0("Daten aufbereitet ", today(), ".csv"), sep = ";")
