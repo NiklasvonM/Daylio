95ab0b6c4e74d6e0636b7ee4306a0fd7035758d0
NiklasvonM <niklas.von.moers2@t-online.de>
2022-05-15 13:07:45 +0200

    init

---
+++ config.R
@@ -0,0 +1,7 @@
+# Whether or not to calculate the distribution with and without an activity.
+# Currently very slow. Only set to TRUE if you want to view that graph.
+CALCULATE_DISTR_WWO_ACTIVITY <- FALSE
+
+fileName <- 
+  #"mockdata"
+  "Daten aufbereitet 2022-05-10"
